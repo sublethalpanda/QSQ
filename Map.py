5e82850a1d70aef5f374da56b57a3c666051d77f
ethentianknight <ethentianknight@gmail.com>
2014-05-05 22:54:59 -0700

    MAAAAAAAAAAPPPPPPSSSSSS
    Print function for whenever combat is to be initiated.

--- Map.py
+++ Map.py
@@ -1,3 +1,4 @@
+from random import randint
 position = [2,1]
 
 #Test Input
@@ -11,7 +12,6 @@ def main():
         flag = True
     userIn = userIn[0:1]
     checkDirect(userIn)
-    print(position)
 
 def room(x,y,direc):
     global flag
@@ -79,6 +79,7 @@ def room(x,y,direc):
             return True
     elif position ==[0,4]:
         #Key Room
+        print("This is where the key is")
         if direc == "n":
             return False
         else:
@@ -87,9 +88,11 @@ def room(x,y,direc):
         if direc == "w":
             return False
         else:
+            print("Initiate miniboss here")
             return True
     elif position == [2,4]:
         #Rest area
+        print("This is the resting room")
         if direc == "n":
             return True
         else:
@@ -115,6 +118,7 @@ def room(x,y,direc):
         if direc == "w":
             return False
     elif position == [4,5]:
+        print("You need a key to continue")
         if direc == "e":
             return False
         else:
@@ -122,13 +126,29 @@ def room(x,y,direc):
     elif position == [2,6]:
         return True
     elif position == [4,6]:
+        print("Boss here.")
         return True
     else:
         return False
     return True
 
 def roomDesc(x,y):
-    descList = [[None,None,"20","30"],["01","11","21","31","41"],[None,"12","22",None,"42"],["03","13","23","33","43"],["04","14","24","34","44"],[None,"15","25","35","45"],[None,None,"26",None,"46"]]
+    descList = [[None,None,"The sun shines warmly on your face.","You have entered a dead-end. This ornate purple room has a single exit to the North."],["You have reached a dead-end. Beyond you lies a wall made of stone.","You are at the southwest side of the pillar. A small tunnel leads west, while the main causeway runs East and North.","You are in the main entrance of the cave. Light spills in from the southern exit, while a great pillar stands to your northwest, yielding both north and west passageways. A hall heads East of here as well.","The hall continues, with a smaller passageway branching off to the south.","The hall continues forward and is joined by a larger, crimson hall running North.","The hall ends here, with a portrait that looks much like you. If you knew what you looked like."],[None,"You are at the northwest section of the pillar. passageways branch north, south, and east.","You are at the northeast section of the pillar. You feel a warm breeze move through the cave here, soothing you.\nPathways lead North,South,and West from here.",None,"The hall becomes a truer hue of crimson, and ornate gold lones paint the ceiling above you."],["""You have found the Cartographer's Room!
+          O         T
+          |         |
+     O----O----O    O
+     |    |    |    |
+O----O    R    O    O
+     |    |    |    |
+O----O    O----O    O
+     |    |         |
+     O----O         O
+     |    |         |
+O----O----O----O----O----O
+          |    |
+          X    O
+This is painted on one of the walls.
+    ""","The air around you smells of old sushi and stale Baleen Bits.\nPathways lead North, South, and West.","You can feel a warmth from the North. The path splits off here, going from a n/s hall to an eastward tunnel.","The tunnel grows dim and heads North and West.","You feel a chill go down your spine as you continue through the dim hallway."],["You are in a small room with a pedestal.","You are in a large antechamber filled halfway with water.","You are in the resting room. A small ever-burning fire warms the floor, which is covered with a soft, snow-white layer of grass.","The tunnel continues North and South.","You can see a large red door far ahead of you. The hall continues."],[None,"The pathway turns sharply. Paths are to the East and South.","There is a junction to the North here.","The air around you smells of something or other. The path from the West continues South as a rough cave.","You are at the bright red door."],[None,None,"Nothing here. Other than you, of course.",None,"You are in the dragon's den!"]]
     print(str(descList[y][x]))
 
 def checkDirect(userIn):
@@ -162,6 +182,9 @@ def checkDirect(userIn):
         print("That is not a direction!")
     if blockFlag == True:
         print("You can't go that way!")
+    else:
+        if randint(1,3) == 1:
+            print("Initiate combat here plz.")
 flag = False
 print("Pick a direction. type 'exit' to leave.")
 while flag == False:
