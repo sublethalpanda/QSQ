eb628b2573dce0c7df5c570b43d06dd64f7bd3a7
ethentianknight <ethentianknight@gmail.com>
2014-05-07 09:43:09 -0700

    User-friendly strings!

--- Map.py
+++ Map.py
@@ -56,10 +56,10 @@ def room(x,y,direc):
             if Globals.whaleTrap == False:
                 Globals.whaleTrap = True
                 if randint(1,20)+Globals.player.DEX >= 20:
-                    print("You narrowly dodge a large form falling from the ceiling.\nA whale has fallen to the ground, covering the walls with bits of Baleen.")
+                    print("\nYou narrowly dodge a large form falling from the ceiling.\nA whale has fallen to the ground, covering the walls with bits of Baleen.")
                 else:
-                    tempDamage = randint(1,200)+50
-                    print("You are hit by a whale falling from the ceiling for",tempDamage,"damage.")
+                    tempDamage = randint(1,200)+50+randint(1,Globals.player.HP[1] - 1)
+                    print("\nYou are hit by a whale falling from the ceiling for",tempDamage,"damage.")
                     Globals.player.HP[0] -=tempDamage
                     if Globals.player.dead():
                         print("Game Over...")
@@ -142,14 +142,14 @@ def room(x,y,direc):
     return True
 
 def roomDesc(x,y):
-    descList = [[None,None,"The sun shines warmly on your face.","You have entered a dead-end. This ornate purple room has a single exit to the North."],["You have reached a dead-end. Beyond you lies a wall made of stone.","You are at the southwest side of the pillar. A small tunnel leads west, while the main causeway runs East and North.","You are in the main entrance of the cave. Light spills in from the southern exit, while a great pillar stands to your northwest, yielding both north and west passageways. A hall heads East of here as well.","The hall continues, with a smaller passageway branching off to the south.","The hall continues forward and is joined by a larger, crimson hall running North.","The hall ends here, with a portrait that looks much like you. If you knew what you looked like."],[None,"You are at the northwest section of the pillar. passageways branch north, south, and east.","You are at the northeast section of the pillar. You feel a warm breeze move through the cave here, soothing you.\nPathways lead North,South,and West from here.",None,"The hall becomes a truer hue of crimson, and ornate gold lones paint the ceiling above you."],["""You have found the Cartographer's Room!
+    descList = [[None,None,"\nThe sun shines warmly on your face.","\nYou have entered a dead-end.\nThis ornate purple room has a single exit to the North."],["\nYou have reached a dead-end.\nBeyond you lies a wall made of stone.","\nYou are at the southwest side of the pillar.\nA small tunnel leads west, while the main causeway runs East and North.","\nYou are in the main entrance of the cave.\nLight spills in from the southern exit, while a great pillar stands to\nyour northwest, yielding both north and west passageways.\nA hall heads East of here as well.","\nThe hall continues, with a smaller passageway branching off to the south.","\nThe hall continues forward and is joined by a\nlarger, crimson hall running North.","\nThe hall ends here, with a portrait that looks much like you.\nIf you knew what you looked like."],[None,"\nYou are at the northwest section of the pillar.\nPassageways branch north, south, and east.","\nYou are at the northeast section of the pillar.\nYou feel a warm breeze move through the cave here, soothing you.\nPathways lead North,South,and West from here.",None,"\nThe hall becomes a truer hue of crimson, and\nornate gold lones paint the ceiling above you."],["""You have found the Cartographer's Room!
           O         T
           |         |
      O----O----O    O
+     |         |    |
+O----M    R    O    O
      |    |    |    |
-O----O    R    O    O
-     |    |    |    |
-O----O    O----O    O
+C----O    O----O    O
      |    |         |
      O----O         O
      |    |         |
@@ -157,7 +157,7 @@ O----O----O----O----O----O
           |    |
           X    O
 This is painted on one of the walls.
-    ""","The air around you smells of old sushi and stale Baleen Bits.\nPathways lead North, South, and West.","You can feel a warmth from the North. The path splits off here, going from a n/s hall to an eastward tunnel.","The tunnel grows dim and heads North and West.","You feel a chill go down your spine as you continue through the dim hallway."],["You are in a small room with a pedestal.","You are in a large antechamber filled halfway with water.","You are in the resting room. A small ever-burning fire warms the floor, which is covered with a soft, snow-white layer of grass.","The tunnel continues North and South.","You can see a large red door far ahead of you. The hall continues."],[None,"The pathway turns sharply. Paths are to the East and South.","There is a junction to the North here.","The air around you smells of something or other. The path from the West continues South as a rough cave.","You are at the bright red door."],[None,None,"Nothing here. Other than you, of course.",None,"You are in the dragon's den!"]]
+    ""","\nThe air around you smells of old sushi and stale Baleen Bits.\nPathways lead North, South, and West.","\nYou can feel a warmth from the North.\nThe path splits off here, going from a n/s hall to an eastward tunnel.","\nThe tunnel grows dim and heads North and West.","\nYou feel a chill go down your spine as you continue through the dim hallway."],["\nYou are in a small room with a pedestal.","\nYou are in a large antechamber filled halfway with water.","\nYou are in the resting room. A small ever-burning fire warms the floor,\nwhich is covered with a soft, snow-white layer of grass.","\nThe tunnel continues North and South.","\nYou can see a large red door far ahead of you.\nThe hall continues."],[None,"\nThe pathway turns sharply.\nPaths are to the East and South.","\nThere is a junction to the North here.","\nThe air around you smells of something or other.\nThe path from the West continues South as a rough cave.","\nYou are at the bright red door."],[None,None,"\nNothing here.\n\nOther than you, of course.",None,"\nYou are in the dragon's den!"]]
     print(str(descList[y][x]))
 
 def checkDirect(userIn):
